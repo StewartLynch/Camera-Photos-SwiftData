a0ca38462db7a62b18e6a7efca394478d4673dde
Stewart Lynch <slynch@createchsol.com>
2024-03-18 14:50:23 -0700

    Initial Commit

---
+++ Camera Photos SwiftData/Camera_Photos_SwiftDataApp.swift
@@ -0,0 +1,22 @@
+//
+// Created for Camera Photos SwiftData
+// by  Stewart Lynch on 2024-03-18
+//
+// Follow me on Mastodon: @StewartLynch@iosdev.space
+// Follow me on Threads: @StewartLynch (https://www.threads.net)
+// Follow me on X: https://x.com/StewartLynch
+// Follow me on LinkedIn: https://linkedin.com/in/StewartLynch
+// Subscribe on YouTube: https://youTube.com/@StewartLynch
+// Buy me a ko-fi:  https://ko-fi.com/StewartLynch
+
+
+import SwiftUI
+
+@main
+struct Camera_Photos_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
